96254b3b9a655db46a7cd55ab07e53e9b6f9cb37
Stevan Stevic <stevan.stevic@proton.me>
2025-03-02 16:16:52 +0100

    Format setup.py

--- setup.py
+++ setup.py
@@ -4,29 +4,29 @@ import os
 
 from setuptools import setup, find_packages
 
-from esp_flasher import const
+from esp_flasher.core import const
 
-PROJECT_NAME = 'ESP32 GUI Flash Download Tool'
-PROJECT_PACKAGE_NAME = 'esp32_gui_flasher'
-PROJECT_LICENSE = 'MIT'
-PROJECT_AUTHOR = 'Stevan Stevic'
-PROJECT_COPYRIGHT = '2025, Stevan Stevic'
-PROJECT_URL = 'https://github.com/stevanStevic/esp32-gui-flasher'
-PROJECT_EMAIL = 'stevan.stevic@proton.me'
+PROJECT_NAME = "ESP32 GUI Flash Download Tool"
+PROJECT_PACKAGE_NAME = "esp32_gui_flasher"
+PROJECT_LICENSE = "MIT"
+PROJECT_AUTHOR = "Stevan Stevic"
+PROJECT_COPYRIGHT = "2025, Stevan Stevic"
+PROJECT_URL = "https://github.com/stevanStevic/esp32-gui-flasher"
+PROJECT_EMAIL = "stevan.stevic@proton.me"
 
-PROJECT_GITHUB_USERNAME = 'stevanStevic'
-PROJECT_GITHUB_REPOSITORY = 'esp32-gui-flasher'
+PROJECT_GITHUB_USERNAME = "stevanStevic"
+PROJECT_GITHUB_REPOSITORY = "esp32-gui-flasher"
 
-PYPI_URL = 'https://pypi.python.org/pypi/{}'.format(PROJECT_PACKAGE_NAME)
-GITHUB_PATH = '{}/{}'.format(PROJECT_GITHUB_USERNAME, PROJECT_GITHUB_REPOSITORY)
-GITHUB_URL = 'https://github.com/{}'.format(GITHUB_PATH)
+PYPI_URL = "https://pypi.python.org/pypi/{}".format(PROJECT_PACKAGE_NAME)
+GITHUB_PATH = "{}/{}".format(PROJECT_GITHUB_USERNAME, PROJECT_GITHUB_REPOSITORY)
+GITHUB_URL = "https://github.com/{}".format(GITHUB_PATH)
 
 here = os.path.abspath(os.path.dirname(__file__))
 
-with open(os.path.join(here, 'requirements.txt')) as requirements_txt:
+with open(os.path.join(here, "requirements.txt")) as requirements_txt:
     REQUIRES = requirements_txt.read().splitlines()
 
-with open(os.path.join(here, 'README.md')) as readme:
+with open(os.path.join(here, "README.md")) as readme:
     LONG_DESCRIPTION = readme.read()
 
 
@@ -40,17 +40,13 @@ setup(
     description="ESP32 GUI Flash Download Tool with Connection to Thermal Printer.",
     include_package_data=True,
     zip_safe=False,
-    platforms='any',
-    test_suite='tests',
-    python_requires='>=3.8,<4.0',
+    platforms="any",
+    test_suite="tests",
+    python_requires=">=3.8,<4.0",
     install_requires=REQUIRES,
     long_description=LONG_DESCRIPTION,
-    long_description_content_type='text/markdown',
-    keywords=['esp32', 'automation', 'flash'],
-    entry_points={
-        'console_scripts': [
-            'esp_flasher = esp_flasher.__main__:main'
-        ]
-    },
-    packages=find_packages(include="esprelease.*")
+    long_description_content_type="text/markdown",
+    keywords=["esp32", "automation", "flash"],
+    entry_points={"console_scripts": ["esp_flasher = esp_flasher.__main__:main"]},
+    packages=find_packages(include="esprelease.*"),
 )
