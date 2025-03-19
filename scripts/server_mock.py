ad976b270795fc5ae1cdfa586f86d058110a5c66
Stevan Stevic <stevan.stevic@proton.me>
2025-03-19 22:46:33 +0100

    Server mock fix return messages

--- scripts/server_mock.py
+++ scripts/server_mock.py
@@ -8,11 +8,11 @@ def mock_publish():
     data = request.json
     if "mac_address" in data:
         return (
-            jsonify({"status": "success", "device_name": "TEST_NAME"}),
+            jsonify({"device_name": "TEST_NAME"}),
             200,
         )
     else:
-        return jsonify({"status": "error", "message": "Missing MAC address"}), 400
+        return jsonify({"message": "Missing MAC address"}), 400
 
 
 if __name__ == "__main__":
