703ebb6108959eab5037c6e901cfd70695c29b8b
Stevan Stevic <stevan.stevic@proton.me>
2025-03-19 19:55:50 +0100

    Refactor API client

--- esp_flasher/backend/api_client.py
+++ esp_flasher/backend/api_client.py
@@ -1,28 +1,39 @@
 import requests
+from requests.exceptions import RequestException, HTTPError
 
 
-def publish_mac_address(api_endpoint, api_key, api_secret, mac_address):
-    """Send MAC address to API endpoint with authentication."""
+def publish_mac_address(
+    api_endpoint: str, api_key: str, api_secret: str, mac_address: str
+):
+    """Send MAC address to the API endpoint with authentication."""
     headers = {
         "Content-Type": "application/json",
         "X-API-KEY": api_key,
         "X-API-SECRET": api_secret,
     }
-
     payload = {"mac_address": mac_address}
 
     try:
-        response = requests.post(api_endpoint, headers=headers, json=payload)
-        response.raise_for_status()  # Raise exception if HTTP error occurs
+        with requests.Session() as session:
+            response = session.post(api_endpoint, headers=headers, json=payload)
+            response.raise_for_status()  # Raises HTTPError for 4xx/5xx responses
 
-        response_data = response.json()
+            if response.status_code == 201:
+                return response.json().get("device_name", "Unknown Device"), None
 
-        if response_data.get("status") == "success":
-            return response_data.get("device_name", "Unknown Device"), None  # Success
+            return (
+                None,
+                f"Unexpected status code: {response.status_code} {response.reason} - {response_data["message"]}",
+            )
+
+    except HTTPError as http_err:
+        response_data = http_err.response.json()
+
         return (
             None,
-            f"API Error: {response_data.get('message', 'Unknown error from server')}",
+            f"HTTP error: {response.status_code} {response.reason} - {response_data["message"]}",
         )
-
-    except requests.exceptions.RequestException as e:
-        return None, f"Network Error: {e}"
+    except RequestException as req_err:
+        return None, f"Request failed: {req_err}"
+    except Exception as err:
+        return None, f"An unexpected error occurred: {err}"
